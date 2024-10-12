855041de2a8323737311838918bfa1346293943d
heosangbeom <luv_3_u@naver.com>
2024-10-12 12:42:22 +0900

    feat(user): 회원 관련 클래스 추가(security, user)
    - security
       - handler
          - LoginFailureHandler
            - 로그인 실패 처리
    
          - LoginSuccessHandler
            - 로그인 성공 처리
    
     - user
       - domain
         - Users
           - 유저 엔티티
         - UserRole
           - 유저롤 엔티티
    
       - dto
         - JoinRequest
         - UserDto

---
+++ src/main/kotlin/com/bgaebalja/blogbackend/security/handler/LoginSuccessHandler.kt
@@ -0,0 +1,27 @@
+package com.example.kotlinprojecttest.security.handler
+
+import com.bgaebalja.blogbackend.util.generateToken
+import com.example.kotlinprojecttest.user.dto.UserDto
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+
+class LoginSuccessHandler: AuthenticationSuccessHandler {
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        authentication: Authentication?
+    ) {
+        val objectMapper = ObjectMapper()
+        val user: UserDto = authentication?.principal as UserDto
+        val claims = user.getClaims()
+        val accessToken = generateToken(claims, 10L)
+        val refreshToken = generateToken(claims, 60 * 24)
+        claims["access_token"] = accessToken
+        claims["refresh_token"] = refreshToken
+        response?.contentType = "application/json;charset=UTF-8"
+        response?.writer?.write(objectMapper.writeValueAsString(claims))
+    }
+}
