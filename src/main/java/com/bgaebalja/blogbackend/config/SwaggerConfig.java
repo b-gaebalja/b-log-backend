b3ceaef3750929d4163defefcd8e8abb71b1fda1
Sung Hyobin <hellmir610@gmail.com>
2024-10-13 14:47:41 +0900

    chore(swagger): 서버 API 문서화를 위한 Swagger 설정 추가 (#14)

---
+++ src/main/java/com/bgaebalja/blogbackend/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.bgaebalja.blogbackend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("B-log 서버 API")
+                        .description("B-log 프로젝트의 서버 API입니다.")
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                "bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
+    }
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("b-log")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
