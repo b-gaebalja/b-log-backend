e4efea9652d8e12a043e4c8bf8459ba58450a66f
Sung Hyobin <hellmir610@gmail.com>
2024-10-15 22:17:28 +0900

    feat(post): 자신의 게시글 목록 조회 기능 추가 (#41)
    - 자신의 게시글 목록 조회 요청
    - 자신의 게시글 목록 조회 비즈니스 로직
    - 자신의 게시글 목록 데이터 반환
    - 각 게시글의 대표 이미지 목록 조회 비즈니스 로직
    - 각 게시글의 대표 이미지 목록 데이터 반환
    - 200 status code 반환

--- src/main/java/com/bgaebalja/blogbackend/post/controller/PostController.java
+++ src/main/java/com/bgaebalja/blogbackend/post/controller/PostController.java
@@ -1,16 +1,20 @@
 package com.bgaebalja.blogbackend.post.controller;
 
-import com.bgaebalja.blogbackend.post.domain.CompletePostRequest;
-import com.bgaebalja.blogbackend.post.domain.GetPostResponse;
-import com.bgaebalja.blogbackend.post.domain.Post;
-import com.bgaebalja.blogbackend.post.domain.RegisterPostRequest;
+import com.bgaebalja.blogbackend.image.domain.Image;
+import com.bgaebalja.blogbackend.image.domain.RepresentativeImagesRequest;
+import com.bgaebalja.blogbackend.image.service.ImageService;
+import com.bgaebalja.blogbackend.post.domain.*;
 import com.bgaebalja.blogbackend.post.service.PostService;
+import com.bgaebalja.blogbackend.post.util.PageableGenerator;
 import com.bgaebalja.blogbackend.util.FormatValidator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +22,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
-import static com.bgaebalja.blogbackend.util.ApiConstant.ID_EXAMPLE;
+import static com.bgaebalja.blogbackend.post.util.PaginationConstant.*;
+import static com.bgaebalja.blogbackend.util.ApiConstant.*;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
@@ -28,6 +34,7 @@ import static org.springframework.http.HttpStatus.CREATED;
 @Tag(name = "Post Controller", description = "게시글 관련 API")
 public class PostController {
     private final PostService postService;
+    private final ImageService imageService;
 
     private static final String REGISTER_POST = "게시글 임시 등록";
     private static final String REGISTER_POST_DESCRIPTION
@@ -41,7 +48,15 @@ public class PostController {
 
     private static final String GET_POST = "게시글 조회";
     private static final String GET_POST_DESCRIPTION = "게시글 ID를 입력해 게시글을 조회할 수 있습니다.";
-    public static final String POST_ID = "게시글 ID";
+    private static final String POST_ID = "게시글 ID";
+
+    private static final String GET_ALL_POSTS = "전체 게시글 목록 조회";
+    private static final String GET_ALL_POSTS_DESCRIPTION = "전체 게시글 목록을 조회할 수 있습니다." +
+            "\n페이징 옵션을 선택할 수 있습니다.";
+
+    private static final String GET_MY_POSTS = "자신의 게시글 목록 조회";
+    private static final String GET_MY_POSTS_DESCRIPTION = "자신의 게시글 목록을 조회할 수 있습니다." +
+            "\n페이징 옵션을 선택할 수 있습니다.";
 
     @Operation(summary = REGISTER_POST, description = REGISTER_POST_DESCRIPTION)
     @PostMapping()
@@ -85,5 +100,27 @@ public class PostController {
         Post post = postService.getPost(Long.parseLong(id));
 
         return ResponseEntity.status(HttpStatus.OK).body(GetPostResponse.from(post));
+    }
+
+    @Operation(summary = GET_MY_POSTS, description = GET_MY_POSTS_DESCRIPTION)
+    @GetMapping("/users")
+    public ResponseEntity<GetPostsResponse> getMyPosts
+            (@RequestParam(name = "email")
+             @Schema(description = USER_EMAIL_VALUE, example = EMAIL_EXAMPLE) String email,
+             @RequestParam(defaultValue = TRUE)
+             @Schema(description = IS_PAGINATION_USED, example = TRUE) String paged,
+             @RequestParam(defaultValue = ZERO)
+             @Schema(description = CURRENT_PAGE_NUMBER, example = ZERO) String pageNumber,
+             @RequestParam(defaultValue = TWELVE)
+             @Schema(description = NUMBER_OF_ITEMS_PER_PAGE, example = TWELVE) String size,
+             @RequestParam(defaultValue = ORDER_BY_CREATED_AT_DESCENDING)
+             @Schema(description = SORTING_METHOD, example = ORDER_BY_CREATED_AT_DESCENDING) String sort) {
+        FormatValidator.validateEmail(email);
+        Pageable pageable = PageableGenerator.createPageable(paged, pageNumber, size, sort);
+        Page<Post> posts = postService.getPosts(pageable, email);
+        List<Image> representativeImages
+                = imageService.getRepresentativeImages(RepresentativeImagesRequest.from(posts.getContent()));
+
+        return ResponseEntity.status(HttpStatus.OK).body(GetPostsResponse.from(posts, representativeImages));
     }
 }
---
+++ src/main/java/com/bgaebalja/blogbackend/post/util/PaginationConstant.java
@@ -0,0 +1,13 @@
+package com.bgaebalja.blogbackend.post.util;
+
+public class PaginationConstant {
+    public static final String IS_PAGINATION_USED = "페이지네이션 사용 여부";
+    public static final String CURRENT_PAGE_NUMBER = "현재 페이지 번호";
+    public static final String NUMBER_OF_ITEMS_PER_PAGE = "페이지 당 항목 수";
+    public static final String SORTING_METHOD = "정렬 방식";
+
+    public static final String TRUE = "true";
+    public static final String ZERO = "0";
+    public static final String TWELVE = "12";
+    public static final String ORDER_BY_CREATED_AT_DESCENDING = "createdAt,desc";
+}
