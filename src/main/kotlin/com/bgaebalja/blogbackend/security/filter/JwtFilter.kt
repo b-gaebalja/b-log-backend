597a3958f48325986669b9579ce2c965a5fd2a01
heosangbeom <luv_3_u@naver.com>
2024-10-17 11:28:08 +0900

    fix(user): jwt 필터 기능 오류 수정 및 추가
    - jwt 필터 post 추가

--- src/main/kotlin/com/bgaebalja/blogbackend/security/filter/JwtFilter.kt
+++ src/main/kotlin/com/bgaebalja/blogbackend/security/filter/JwtFilter.kt
@@ -3,10 +3,12 @@ package com.bgaebalja.blogbackend.security.filter
 import com.bgaebalja.blogbackend.user.dto.UserDto
 import com.bgaebalja.blogbackend.util.JwtUtil
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.jsonwebtoken.JwtException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
@@ -14,16 +16,21 @@ import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
-class JwtFilter(private val jwtUtil: JwtUtil): OncePerRequestFilter() {
+class JwtFilter(private val jwtUtil: JwtUtil) : OncePerRequestFilter() {
     private fun validatePath(path: String, pathUri: String) = path.startsWith(pathUri)
 
     override fun shouldNotFilter(request: HttpServletRequest): Boolean {
         val path = request.requestURI
+        val method = request.method
         return if (validatePath(path, "/users/")) {
             true
-        } else {
+        } else if (path.equals("/posts/users")) {
             false
-        }
+        } else if (path.equals("/posts") && method.equals(HttpMethod.POST.toString())) {
+            false
+        } else if (validatePath(path, "/posts")) {
+            true
+        } else false
 
     }
 
@@ -34,7 +41,8 @@ class JwtFilter(private val jwtUtil: JwtUtil): OncePerRequestFilter() {
     ) {
         val objectMapper = ObjectMapper()
         val authorizationHeader = request.getHeader(AUTHORIZATION)
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            ?: throw JwtException("Authorization header is missing")
+        if (authorizationHeader.startsWith("Bearer ")) {
             val accessToken = authorizationHeader.substring(7)
             val claims = jwtUtil.validateJwtToken(accessToken) ?: run {
                 val message =
@@ -51,7 +59,7 @@ class JwtFilter(private val jwtUtil: JwtUtil): OncePerRequestFilter() {
             val imageUrl = claims["imageUrl"] as String
             val roles = claims["roles"] as MutableList<String?>
             val password = claims["password"] as String
-            val userDto = UserDto(id, userId, email, username, fullName, password, imageUrl,roles)
+            val userDto = UserDto(id, userId, email, username, fullName, password, imageUrl, roles)
             val authenticationToken =
                 UsernamePasswordAuthenticationToken(userDto, password, userDto.authorities)
             SecurityContextHolder.getContext().authentication = authenticationToken
