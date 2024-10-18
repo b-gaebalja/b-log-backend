cc2bbe9fba9f780185ecb32e275614a7a84ebefc
React-test <gitdoolgi@gmail.com>
2024-10-18 14:13:02 +0900

    fix : CommentRepository 댓글 불러오는 로직 이름 변경

--- src/main/java/com/bgaebalja/blogbackend/comment/repository/CommentRepository.java
+++ src/main/java/com/bgaebalja/blogbackend/comment/repository/CommentRepository.java
@@ -5,13 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Comment findByUserId(Long userId);
-
-    Comment findByCommentId(Long commentId);
+    Optional<Comment> findById(Long id);
     List<Comment> findAllCommentsByPostId(Long postId);
     Long countCommentByPostId(Long postId);
 }
