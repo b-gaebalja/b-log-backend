81a711212aa0fec16ecccbfbb39a7c72fe3e5f1b
React-test <gitdoolgi@gmail.com>
2024-10-21 11:40:35 +0900

    feature : CommentRepository Optional대신 List로 받게 수정

--- src/main/java/com/bgaebalja/blogbackend/comment/repository/CommentRepository.java
+++ src/main/java/com/bgaebalja/blogbackend/comment/repository/CommentRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
-
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Optional<Comment> findById(Long id);
+    List<Comment> findAllById(Long id);
     List<Comment> findAllCommentsByPostId(Long postId);
     Long countCommentByPostId(Long postId);
 }
