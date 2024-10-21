d0335c406e9cbdbf68cbbbba414defca77c235f4
React-test <gitdoolgi@gmail.com>
2024-10-21 11:41:13 +0900

    feature : CommentService 총 댓글 수, 댓글 이동 service 추가

--- src/main/java/com/bgaebalja/blogbackend/comment/service/CommentService.java
+++ src/main/java/com/bgaebalja/blogbackend/comment/service/CommentService.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 public interface CommentService {
     List<Comment> getComments(Long postId);
+    List<Comment> toComments(Long id);
     Long countComments(Long postId);
-    Long createComment(RegisterCommentRequest registerCommentRequest, Long postId);
+    Long createComment(RegisterCommentRequest registerCommentRequest);
     Long modifyComment(RegisterCommentRequest registerCommentRequest, Long commentId);
-
+    Long deleteComment(Long commentId);
 
 }
